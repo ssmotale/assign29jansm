22da325a93674b650782433e58e0aa5f17512011
ssmotale <125238798+ssmotale@users.noreply.github.com>
2023-02-23 10:38:59 +0530

    Add files via upload

---
+++ assignpythonsm.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+2+2
+
+
+# In[ ]:
+
+
+"""
+Q1) who developed python programming language?
+Ans: Guido van Rossum
+
+"""
+
+
+# In[14]:
+
+
+"""
+Q2) which type of programming does python supports
+Ans: object oriented programming and structured programming
+"""
+
+
+# In[15]:
+
+
+"""
+Q3)is python case sensitive when dealing with identifier?
+Ans: yes it is case sensitive 
+"""
+
+
+# In[16]:
+
+
+"""
+Q3)what is correct extension of python file
+Ans: .py is correct extension of pytho file
+"""
+
+
+# In[17]:
+
+
+"""
+Q4)is python code compiled or interpreted
+Ans:interpreted
+"""
+
+
+# In[18]:
+
+
+"""
+Q5)name few block of code to define python language
+
+Ans:module ,function body and class definition are 3 basic building blocks of python language
+
+"""
+
+
+# In[19]:
+
+
+"""
+Q6)State character used to give single line comment in python?
+Ans:"#" is character used to give single line comment
+
+"""
+
+
+# In[20]:
+
+
+"""
+Q7)
+mention function which can help us to find the version of python that we are currently working on
+"""
+import sys
+print("User Current Version:-", sys.version)
+
+
+# In[23]:
+
+
+"""
+Q8)python supports the creation of anonymous functions at runtime using construct called ___
+
+Ans:lambda
+
+"""
+
+
+# In[24]:
+
+
+"""
+Q9)what does pip stand for python
+Ans:preffered installer programme
+
+"""
+
+
+
+# In[ ]:
+
+
+"""
+Q10)mention few built in functions in python
+
+Ans:print(),max(),min(),type()
+
+"""
+
+
+# In[ ]:
+
+
+"""
+Q11)what is the max possible length of an identifier
+
+Ans: 79 characters 
+"""
+
+
+# In[ ]:
+
+
+"""
+Q12)what are benefits of using python
+
+Ans:cocise syntax,extensive libraries
+"""
+
+
+# In[ ]:
+
+
+"""
+Q13)what are benefits of using python
+
+Ans:cocise syntax,extensive libraries
+"""
+
+
+# In[ ]:
+
+
+"""
+Q14)how is memory managed in python
+
+Ans:Memory management in Python involves a private heap containing all Python objects and data structures. 
+"""
+
+
+# In[ ]:
+
+
+"""
+Q15)how is memory managed in python
+
+Ans:Memory management in Python involves a private heap containing all Python objects and data structures. 
+"""
+
+
+# In[25]:
+
+
+"""
+Q16)is indentation required in python
+
+Ans:Yes indentation is required. 
+"""
+
+
+# In[ ]:
+
+
+
+
